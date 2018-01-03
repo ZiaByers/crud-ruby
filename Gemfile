407aa1b18e20b9bebe78ecbea6737276f5159624
Zach Byers <byerszach@gmail.com>
2018-01-03 10:40:20 -0700

    index, show, almost create

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+ruby '2.3.1'
+
+gem 'httparty', '~> 0.15.6'
+gem 'pry', '~> 0.11.3'
---
+++ crud-ruby.rb
@@ -0,0 +1,86 @@
+require 'httparty'
+require 'pry'
+
+BASE_URL = 'http://json-server.devpointlabs.com/api/v1'
+
+def print_user(user)
+  puts "id: #{user['id']}"
+  puts "First Name: #{user['first_name']}"
+  puts "Last Name: #{user['last_name']}"
+  puts "Phone Number: #{user['phone_number']}"
+  puts ''
+end
+
+def all_users
+  users = HTTParty.get("#{BASE_URL}/users")
+
+  users.each do |user|
+    print_user(user)
+  end
+end
+
+def single_user
+  puts "Enter desired ID"
+  id = gets.to_i
+
+  user = HTTParty.get("#{BASE_URL}/users/#{id}")
+
+  error_message = user.parsed_response["message"]
+
+  if error_message
+    puts error_message
+  else
+    print_user(user)
+  end
+
+end
+
+def create_user
+  puts "Enter First Name"
+  first_name = gets.strip
+
+  puts "Enter Last Name"
+  last_name = gets.strip
+
+  puts "Enter Phone Number"
+
+  phone_number = gets.strip
+
+  user = {
+    first_name: first_name,
+    last_name: last_name,
+    phone_number: phone_number
+  }
+
+  response = HTTParty.post("#{BASE_URL}/users", body: user)
+
+  puts response
+end
+
+while true
+  puts """
+  1. List all users
+  2. Find user by id
+  3. Create new user
+  4. Update user by id
+  5. Delete user by id
+  6. Quit
+  """
+  input = gets.strip.to_i
+
+  case input
+    when 1
+      all_users
+    when 2
+      single_user
+    when 3
+      create_user
+    when 4
+    when 5
+    when 6
+      puts "See ya"
+      exit 0
+    else
+      puts "Invalid input, try again."
+  end
+end
